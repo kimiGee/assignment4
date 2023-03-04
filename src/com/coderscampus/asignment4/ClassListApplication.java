89c7a0b0baec912837666d87ccf6fb6f71b9be5f
kimiGee <kimberlyreneegonzales@gmail.com>
2023-03-04 10:08:59 -0500

    implemented arrays

--- src/com/coderscampus/asignment4/ClassListApplication.java
+++ src/com/coderscampus/asignment4/ClassListApplication.java
@@ -6,14 +6,16 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
 public class ClassListApplication {
-	
-	ArrayList<StudentInfo> apmthList = new ArrayList<>();
-	ArrayList<StudentInfo> statList = new ArrayList<>();
-	ArrayList<StudentInfo> compsciList = new ArrayList<>();
+
+	StudentInfo[] allStudents = new StudentInfo[105];
+	StudentInfo[] compsci = new StudentInfo[35];
+	StudentInfo[] stat = new StudentInfo[35];
+	StudentInfo[] math = new StudentInfo[35];
 
 	public static void main(String args[]) throws IOException {
 
@@ -22,11 +24,13 @@ public class ClassListApplication {
 		list.sortClassListsByGrade();
 
 	}
-	
-	//Read file contents from student-master-list.csv
+
+	// Read file contents from student-master-list.csv, create StudentInfo object
+	// and add to allStudents array
 	public void readMasterList() {
 		String fileName = "student-master-list.csv";
 		BufferedReader reader = null;
+		Integer counter = 0;
 
 		try {
 			reader = new BufferedReader(new FileReader(fileName));
@@ -37,7 +41,8 @@ public class ClassListApplication {
 				try {
 					grade = Integer.parseInt(listEntry[3]);
 					StudentInfo student = new StudentInfo(listEntry[0], listEntry[1], listEntry[2], grade);
-					sortStudents(student);
+					allStudents[counter] = student;
+					counter++;
 				} catch (NumberFormatException nfe) {
 					System.out.println("NumberFormat Exception: invalid input string");
 				}
@@ -47,47 +52,52 @@ public class ClassListApplication {
 			e.printStackTrace();
 		}
 
+		this.sortStudents();
 	}
 
-	//Sort StudentInfo objects into appropriate lists according to course
-	public void sortStudents(StudentInfo student) {
-		String course = student.getCourse();
-		if (course.startsWith("COMP")) {
-			compsciList.add(student);
-		} else if (course.startsWith("STA")) {
-			statList.add(student);
-		} else if (course.startsWith("APM")) {
-			apmthList.add(student);
+	// Sort StudentInfo objects from allStudents array into appropriate arrays
+	// according to course
+	public void sortStudents() {
+
+		Integer compCounter = 0;
+		Integer statCounter = 0;
+		Integer mathCounter = 0;
+
+		for (StudentInfo entry : allStudents) {
+			try {
+				String course = entry.getCourse();
+
+				if (course.startsWith("COMP")) {
+					compsci[compCounter] = entry;
+					compCounter++;
+				} else if (course.startsWith("STA")) {
+					stat[statCounter] = entry;
+					statCounter++;
+				} else if (course.startsWith("APM")) {
+					math[mathCounter] = entry;
+					mathCounter++;
+				}
+			} catch (NullPointerException e) {
+				System.out.println("Null pointer exception in allStudents");
+			}
 		}
 
 	}
 
+	// sort each array according to grade
 	public void sortClassListsByGrade() throws IOException {
 
-		String compsci = "course1.csv";		
-		String apmth = "course2.csv";
-		String stat = "course3.csv";
+		String compsciFile = "course1.csv";
+		String mathFile = "course2.csv";
+		String statFile = "course3.csv";
 
-		Collections.sort(compsciList, new Comparator<StudentInfo>() {
+		Arrays.sort(compsci, new Comparator<StudentInfo>() {
 			@Override
 			public int compare(StudentInfo student1, StudentInfo student2) {
 
-				if (student1.getGrade() == student2.getGrade()) {
+				if ((student1 == null) || (student2 == null)) {
 					return 0;
-				} else if (student1.getGrade() < student2.getGrade()) {
-					return 1;
-				} else {
-					return -1;
 				}
-			}
-		});
-		
-		writeToFile(compsci, compsciList);
-
-		Collections.sort(apmthList, new Comparator<StudentInfo>() {
-			@Override
-			public int compare(StudentInfo student1, StudentInfo student2) {
-
 				if (student1.getGrade() == student2.getGrade()) {
 					return 0;
 				} else if (student1.getGrade() < student2.getGrade()) {
@@ -98,12 +108,15 @@ public class ClassListApplication {
 			}
 		});
 
-		writeToFile(apmth, apmthList);
+		writeToFile(compsciFile, compsci);
 
-		Collections.sort(statList, new Comparator<StudentInfo>() {
+		Arrays.sort(math, new Comparator<StudentInfo>() {
 			@Override
 			public int compare(StudentInfo student1, StudentInfo student2) {
 
+				if ((student1 == null) || (student2 == null)) {
+					return 0;
+				}
 				if (student1.getGrade() == student2.getGrade()) {
 					return 0;
 				} else if (student1.getGrade() < student2.getGrade()) {
@@ -114,12 +127,32 @@ public class ClassListApplication {
 			}
 		});
 
-		writeToFile(stat, statList);
+		writeToFile(mathFile, math);
+
+		Arrays.sort(stat, new Comparator<StudentInfo>() {
+			@Override
+			public int compare(StudentInfo student1, StudentInfo student2) {
+
+				if ((student1 == null) || (student2 == null)) {
+					return 0;
+				}
+				if (student1.getGrade() == student2.getGrade()) {
+					return 0;
+				} else if (student1.getGrade() < student2.getGrade()) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+		});
+
+		writeToFile(statFile, stat);
 
 	}
 
-    //write sorted ArrayLists to specified files: course1.csv, course2.csv, and course3.csv
-	public void writeToFile(String fileName, ArrayList<StudentInfo> list) throws IOException {
+	// write sorted array to specified files: course1.csv, course2.csv, and
+	// course3.csv
+	public void writeToFile(String fileName, StudentInfo[] list) throws IOException {
 
 		BufferedWriter writer = null;
 		String headers = "Student ID, Student Name, Course, Grade";
@@ -128,10 +161,13 @@ public class ClassListApplication {
 			writer = new BufferedWriter(new FileWriter(fileName));
 			writer.write(headers + "\n");
 			for (StudentInfo entry : list) {
-
-				String csvEntry = entry.getID() + "," + entry.getName() + "," + entry.getCourse() + ","
-						+ entry.getGrade();
-				writer.write(csvEntry + "\n");
+				try {
+					String csvEntry = entry.getID() + "," + entry.getName() + "," + entry.getCourse() + ","
+							+ entry.getGrade();
+					writer.write(csvEntry + "\n");
+				} catch (NullPointerException e) {
+					System.out.println("Null Pointer exception");
+				}
 			}
 
 		} finally {
